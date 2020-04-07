1fbdad304848f931986c79cafebd52f07bacab3c
Kendra Zolner <kendra@dijatek.com>
2020-04-07 16:00:41 -0400

    copy-pasta node-cors

--- index.js
+++ index.js
@@ -3,17 +3,45 @@ var cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(cors())
-app.options('*', cors()) // enable pre-flight across the board
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
+/* -------------------------------------------------------------------------- */
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.post('/', (req, res) => {
-    console.log(req.params)
-    res.json(req.params)
-})
+app.get("/no-cors", (req, res) => {
+    console.info("GET /no-cors");
+    res.json({
+      text: "You should not see this via a CORS request."
+    });
+  });
+
+  /* -------------------------------------------------------------------------- */
+
+  app.head("/", cors(), (req, res) => {
+    console.info("HEAD /");
+    res.sendStatus(204);
+  });
+  app.get("/", cors(), (req, res) => {
+    console.info("GET / - simple-cors");
+    res.json({
+      text: "Simple CORS requests are working. [GET]"
+    });
+  });
+  app.post("/", cors(), (req, res) => {
+    console.info("POST / - simple-cors");
+    res.json({
+      text: "Simple CORS requests are working. [POST]"
+    });
+  });
+
+  /* -------------------------------------------------------------------------- */
+
+  app.options("/complex-cors", cors());
+  app.delete("/complex-cors", cors(), (req, res) => {
+    console.info("DELETE /complex-cors");
+    res.json({
+      text: "Complex CORS requests are working. [DELETE]"
+    });
+  });
+
+  /* -------------------------------------------------------------------------- */
+
+
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
