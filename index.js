5e7faebdb0d2cb8144722203d9c54e3ac33a4515
Kendra Zolner <kendra@dijatek.com>
2020-04-07 15:51:17 -0400

    add cors pacakge

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(cors())
+app.options('*', cors()) // enable pre-flight across the board
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.post('/', (req, res) => {
