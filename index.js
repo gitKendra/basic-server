17cc573fe5cbbe0515a754cb365d7100ca6780e0
Kendra Zolner <kendra@dijatek.com>
2020-04-07 15:25:05 -0400

    make it better

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.post('/', (req, res) => {
+    console.log(req.params)
+    res.json(req.params)
+})
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
