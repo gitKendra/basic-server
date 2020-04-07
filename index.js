ceccb666f1fa37126656cf2e4ded1f4abd0141b8
Kendra Zolner <kendra@dijatek.com>
2020-04-07 15:28:16 -0400

    update url

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "http://dijatek.net.s3-website.us-east-2.amazonaws.com/"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
